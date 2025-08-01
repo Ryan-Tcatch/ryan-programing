63acc52f1c1cdee0b8ec0c4e4ec467a6df800117
Ryan-Tcatch <tacatryan@gmail.com>
2025-08-01 19:51:38 -0300

    Adicionando verificador de palíndromos

--- CursoExercícios/Sorteador/Sorteador-projeto_inicial/exercicios a parte/palindromo/app.js
+++ CursoExercícios/Sorteador/Sorteador-projeto_inicial/exercicios a parte/palindromo/app.js
@@ -1,7 +1,22 @@
-let palavra = document.getElementById('wrd').textContent
 
-function ordenar() {
-    for (i = 0; i > palavra.length; i++) {
-        console.log(palavra[i])
+function verificar() {
+    let Texto = document.getElementById('Wrd').value;
+    let Resultado = document.getElementById('resultado');
+    if (verificarPalindromo(Texto)) {
+        console.log('é um palindromo');
+        Resultado.innerText = 'É um palíndromo';
+    } else {
+        console.log('não é um palindromo');
+        Resultado.innerText = 'Não é um palíndromo';
     }
 }
+
+function verificarPalindromo(Texto) {
+    let formatada = Texto.toLowerCase();
+    formatada = formatada.replace(/[^a-z0-9]/g, '');
+    console.log(formatada);
+    let invertida = formatada.split('').reverse().join('');
+    console.log(formatada);
+    return formatada == invertida;
+}
+
